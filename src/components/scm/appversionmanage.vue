1986ffc69c5adf23f092859580bd9416e62ee8b4
xcy <xcy_0820@163.com>
2022-10-11 01:02:47 +0800

    package update

---
+++ src/components/scm/appversionmanage.vue
@@ -0,0 +1,22 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import { reactive, ref } from 'vue';
+import { scmlogin, scmtesttoken } from './scmapi';
+
+export default {
+    name: "AppVersionManage",
+    setup() {
+
+
+
+        return {}
+    }
+}
+</script>
+
+<style>
+
+</style>
